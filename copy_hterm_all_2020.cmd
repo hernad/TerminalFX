6c9c339644f22a3143474da21fa8e7e51c1b8670
Ernad Husremovic <hernad@bring.out.ba>
2020-06-17 12:33:17 +0200

    update

---
+++ copy_hterm_all_2020.cmd
@@ -0,0 +1,3 @@
+REM copy  src\main\resources\hterm_all_2020.js
+
+node_modules\.bin\minify  --js libapps\hterm\dist\js\hterm_all.js 2> src/main/resources/hterm_all_2020.min.js
---
+++ set_classpath.cmd
@@ -0,0 +1,24 @@
+set CLASSPATH=
+set MAVEN_LOCAL_REPOS=C:\Users\ernad.husremovic\.m2\repository
+
+set CLASSPATH=%CLASSPATH%;%MAVEN_LOCAL_REPOS%\net\java\dev\jna\jna\5.5.0\jna-5.5.0.jar
+set CLASSPATH=%CLASSPATH%;%MAVEN_LOCAL_REPOS%\net\java\dev\jna\jna-platform\5.5.0\jna-platform-5.5.0.jar
+set CLASSPATH=%CLASSPATH%;%MAVEN_LOCAL_REPOS%\log4j\log4j\1.2.14\log4j-1.2.14.jar
+set CLASSPATH=%CLASSPATH%;%MAVEN_LOCAL_REPOS%\com\google\guava\guava\25.1-jre\guava-25.1-jre.jar
+
+
+
+set OPENJFX_LIB=c:\openjfx\javafx-sdk-14.0.1\lib
+
+set CLASSPATH=%CLASSPATH%;%OPENJFX_LIB%\javafx-swt.jar
+set CLASSPATH=%CLASSPATH%;%OPENJFX_LIB%\javafx.base.jar
+set CLASSPATH=%CLASSPATH%;%OPENJFX_LIB%\javafx.controls.jar
+set CLASSPATH=%CLASSPATH%;%OPENJFX_LIB%\javafx.fxml.jar
+set CLASSPATH=%CLASSPATH%;%OPENJFX_LIB%\javafx.graphics.jar
+set CLASSPATH=%CLASSPATH%;%OPENJFX_LIB%\javafx.media.jar
+set CLASSPATH=%CLASSPATH%;%OPENJFX_LIB%\javafx.properties
+set CLASSPATH=%CLASSPATH%;%OPENJFX_LIB%\javafx.swing.jar
+set CLASSPATH=%CLASSPATH%;%OPENJFX_LIB%\javafx.web.jar
+
+
+REM MAVEN_OPTS="-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=127.0.0.1:8000"
