cc7d24e0a348866c7af394787add28e82b5626ba
Ernad Husremovic <hernad@bring.out.ba>
2020-06-17 10:47:21 +0200

    set_classpath for debug

---
+++ set_classpath.cmd
@@ -0,0 +1,4 @@
+set CLASSPATH=C:\Users\ernad.husremovic\.m2\repository\net\java\dev\jna\jna\5.5.0\jna-5.5.0.jar
+set CLASSPATH=%CLASSPATH%;C:\Users\ernad.husremovic\.m2\repository\net\java\dev\jna\jna-platform\5.5.0\jna-platform-5.5.0.jar
+set CLASSPATH=%CLASSPATH%;C:\Users\ernad.husremovic\.m2\repository\log4j\log4j\1.2.14\log4j-1.2.14.jar
+set CLASSPATH=%CLASSPATH%;C:\Users\ernad.husremovic\.m2\repository\com\google\guava\guava\25.1-jre\guava-25.1-jre.jar
